8b61ad4a11804e21566925c98f1a0a00b842c62b
twinaphex <libretro@gmail.com>
2014-04-08 05:03:42 +0200

    (Android JNI) Fix Mednafen PSX build

--- jni/Android.mk
+++ jni/Android.mk
@@ -47,7 +47,7 @@ ifeq ($(core), psx)
 	NEED_THREADING = 1
 	NEED_TREMOR = 1
    CORE_DEFINE := -DWANT_PSX_EMU
-   CORE_DIR := $(MEDNAFEN_DIR)/psx
+   CORE_DIR := $(MEDNAFEN_DIR)/psx-0932
    CORE_SOURCES := $(CORE_DIR)/psx.cpp \
 	$(CORE_DIR)/irq.cpp \
 	$(CORE_DIR)/timer.cpp \
