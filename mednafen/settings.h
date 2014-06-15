e812ada6693b3facf7d99e6ebc11933184449e28
twinaphex <libretro@gmail.com>
2014-06-15 07:52:27 +0200

    Cleanup settings.h

--- mednafen/settings.h
+++ mednafen/settings.h
@@ -3,27 +3,10 @@
 
 #include <string>
 
-#if defined(WANT_PCE_FAST_EMU)
-extern int setting_pce_fast_nospritelimit;
-extern int setting_pce_fast_cddavolume;
-extern int setting_pce_fast_adpcmvolume;
-extern int setting_pce_fast_cdpsgvolume;
-extern uint32_t setting_pce_fast_cdspeed;
-extern uint32_t setting_pce_keepaspect;
-#elif defined(WANT_PSX_EMU)
 extern uint32_t setting_psx_multitap_port_1;
 extern uint32_t setting_psx_multitap_port_2;
 extern uint32_t setting_psx_analog_toggle;
 extern uint32_t setting_psx_fastboot;
-#elif defined(WANT_NGP_EMU)
-extern uint32_t setting_ngp_language;
-#elif defined(WANT_GBA_EMU)
-extern uint32_t setting_gba_hle;
-#elif defined(WANT_VB_EMU)
-extern uint32_t setting_vb_lcolor;
-extern uint32_t setting_vb_rcolor;
-extern uint32_t setting_vb_anaglyph_preset;
-#endif
 
 bool MDFN_LoadSettings(const char *path, const char *section = NULL, bool override = false);
 bool MDFN_MergeSettings(const void*);
