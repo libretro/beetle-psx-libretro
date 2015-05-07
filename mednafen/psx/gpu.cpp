128b11831354375233f1c1c8921ac3e8a343e4ba
twinaphex <libretro@gmail.com>
2015-05-07 06:46:27 +0200

    (0.9.38.3) Backport to gpu.cpp

--- mednafen/psx/gpu.cpp
+++ mednafen/psx/gpu.cpp
@@ -191,7 +191,7 @@ void PS_GPU::FillVideoParams(MDFNGI* gi)
    // For Justifier and Guncon.
    //
    gi->mouse_scale_x = (float)gi->lcm_width / gi->nominal_width;
-   gi->mouse_offs_x = 0;
+   gi->mouse_offs_x = (float)(2800 - gi->lcm_width) / 2;
 
    gi->mouse_scale_y = 1.0;
    gi->mouse_offs_y = LineVisFirst;
