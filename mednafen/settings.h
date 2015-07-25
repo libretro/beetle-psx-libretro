12bd9f06c08ab63b6ccb1f40b55120cd5177b65c
twinaphex <libretro@gmail.com>
2015-07-25 19:22:49 +0200

    Cleanup

--- mednafen/settings.h
+++ mednafen/settings.h
@@ -12,13 +12,6 @@ extern int setting_initial_scanline_pal;
 extern int setting_last_scanline;
 extern int setting_last_scanline_pal;
 
-bool MDFN_LoadSettings(const char *path, const char *section = NULL, bool override = false);
-bool MDFN_MergeSettings(const void*);
-bool MDFN_MergeSettings(const std::vector<void> &);
-bool MDFN_SaveSettings(const char *path);
-
-void MDFN_KillSettings(void);	// Free any resources acquired.
-
 // This should assert() or something if the setting isn't found, since it would
 // be a totally tubular error!
 uint64 MDFN_GetSettingUI(const char *name);
