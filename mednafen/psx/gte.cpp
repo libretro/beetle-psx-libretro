44329dc521819a75f566b05bc6951c07403ec390
Alcaro <Alcaro@users.noreply.github.com>
2015-07-25 18:42:27 +0200

    Update gte.cpp

--- mednafen/psx/gte.cpp
+++ mednafen/psx/gte.cpp
@@ -702,10 +702,10 @@ uint32_t GTE_ReadDR(unsigned int which)
 
 static INLINE int64_t A_MV(unsigned which, int64_t value)
 {
-   if(value >= (INT32_C(1) << 43))
+   if(value >= (INT64_C(1) << 43))
       FLAGS |= 1 << (30 - which);
 
-   if(value < -(INT32_C(1) << 43))
+   if(value < -(INT64_C(1) << 43))
       FLAGS |= 1 << (27 - which);
 
    return sign_x_to_s64(44, value);
