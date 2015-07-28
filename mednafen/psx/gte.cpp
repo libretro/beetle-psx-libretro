bf2dc866cdb414ecb6bf282c266e6bb567a29488
twinaphex <libretro@gmail.com>
2015-07-28 20:51:15 +0200

    Buildfix

--- mednafen/psx/gte.cpp
+++ mednafen/psx/gte.cpp
@@ -1053,7 +1053,7 @@ static INLINE uint32_t Divide(uint32_t dividend, uint32_t divisor)
 
 static INLINE void TransformXY(int64_t h_div_sz)
 {
-   MAC[0] = F((int64)OFX + IR1 * h_div_sz * (widescreen_hack) ? 0.75 : 1.00) >> 16;
+   MAC[0] = F((int64)OFX + IR1 * h_div_sz * ((widescreen_hack) ? 0.75 : 1.00)) >> 16;
    XY_FIFO[3].X = Lm_G(0, MAC[0]);
 
    MAC[0] = F((int64)OFY + IR2 * h_div_sz) >> 16;
