a553a7fbd48711f901f4801adc55e0c476a01ad1
twinaphex <libretro@gmail.com>
2015-07-30 00:07:56 +0200

    (QNX) Buildfix

--- mednafen/clamp.h
+++ mednafen/clamp.h
@@ -1,6 +1,15 @@
 #ifndef __MDFN_CLAMP_H
 #define __MDFN_CLAMP_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>
+#ifndef _WIN32
+#include <unistd.h>
+#endif
+
 static INLINE int32 clamp_to_u8(int32 i)
 {
  if(i & 0xFFFFFF00)
@@ -29,5 +38,9 @@ static INLINE void clamp(int32_t *val, ssize_t min, ssize_t max)
 #define clamp_simple(val) \
    if ( (int16_t)val != val ) \
       val = (val >> 31) ^ 0x7FFF;
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
