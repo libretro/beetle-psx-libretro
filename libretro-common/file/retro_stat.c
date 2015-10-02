7f9af4d65c2df047350c185c38497a723b2e7264
twinaphex <libretro@gmail.com>
2015-10-02 18:27:52 +0200

    Update

--- libretro-common/file/retro_stat.c
+++ libretro-common/file/retro_stat.c
@@ -101,7 +101,7 @@ static bool path_stat(const char *path, enum stat_mode mode, int32_t *size)
        return false;
 #elif defined(_WIN32)
    WIN32_FILE_ATTRIBUTE_DATA file_info;
-   DWORD ret = GetFileAttributesEx(path, 0, &file_info);
+   DWORD ret = GetFileAttributesEx(path, GetFileExInfoStandard, &file_info);
    if (ret == INVALID_FILE_ATTRIBUTES)
       return false;
 #else
@@ -168,7 +168,7 @@ bool path_is_valid(const char *path)
 
 int32_t path_get_size(const char *path)
 {
-   int32_t filesize;
+   int32_t filesize = 0;
    if (path_stat(path, IS_VALID, &filesize))
       return filesize;
 
