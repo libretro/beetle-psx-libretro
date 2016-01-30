6be2cdbb0b7c9b0c69c1b6f309db483c46bcab11
twinaphex <libretro@gmail.com>
2016-01-30 18:51:08 +0100

    Comment this out for now - gets rid of issues with higher than 1x upscaling

--- mednafen/video/Deinterlacer.cpp
+++ mednafen/video/Deinterlacer.cpp
@@ -88,7 +88,9 @@ void Deinterlacer::InternalProcess(MDFN_Surface *surface, MDFN_Rect &DisplayRect
 
    *dest_lw = LWBuffer[y];
 
+#if 0
    memcpy(dest, src, LWBuffer[y] * sizeof(T));
+#endif
   }
   else if(DeintType == DEINT_BOB)
   {
