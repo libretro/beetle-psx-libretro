46006a9109002cab649623b9ba43b6345a2becb2
twinaphex <libretro@gmail.com>
2016-03-24 18:58:52 +0100

    Add stub rsx.c

--- rsx.h
+++ rsx.h
@@ -7,16 +7,16 @@
 extern "C" {
 #endif
 
-  void rsx_set_environment(retro_environment_t);
-  void rsx_set_video_refresh(retro_video_refresh_t);
-  void rsx_get_system_av_info(struct retro_system_av_info *);
+  void rsx_set_environment(retro_environment_t cb);
+  void rsx_set_video_refresh(retro_video_refresh_t cb);
+  void rsx_get_system_av_info(struct retro_system_av_info *info);
 
   void rsx_init(void);
   bool rsx_open(bool is_pal);
-  void rsx_close();
-  void rsx_refresh_variables();
-  void rsx_prepare_frame();
-  void rsx_finalize_frame();
+  void rsx_close(void);
+  void rsx_refresh_variables(void);
+  void rsx_prepare_frame(void);
+  void rsx_finalize_frame(void);
 
   void rsx_set_draw_offset(int16_t x, int16_t y);
   void rsx_set_draw_area(uint16_t x, uint16_t y,
