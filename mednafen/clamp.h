1b9a6944b3d91dd4e52a0ca628d36f3b925e7356
twinaphex <libretro@gmail.com>
2016-06-07 04:59:17 +0200

    Cleanups

--- mednafen/clamp.h
+++ mednafen/clamp.h
@@ -10,23 +10,6 @@ extern "C" {
 #include <unistd.h>
 #endif
 
-static INLINE int32 clamp_to_u8(int32 i)
-{
- if(i & 0xFFFFFF00)
-  i = (((~i) >> 30) & 0xFF);
-
- return(i);
-}
-
-
-static INLINE int32 clamp_to_u16(int32 i)
-{
- if(i & 0xFFFF0000)
-  i = (((~i) >> 31) & 0xFFFF);
-
- return(i);
-}
-
 static INLINE void clamp(int32_t *val, ssize_t min, ssize_t max)
 {
    if(*val < min)
@@ -34,10 +17,6 @@ static INLINE void clamp(int32_t *val, ssize_t min, ssize_t max)
    if(*val > max)
       *val = max;
 }
-
-#define clamp_simple(val) \
-   if ( (int16_t)val != val ) \
-      val = (val >> 31) ^ 0x7FFF;
 
 #ifdef __cplusplus
 }
