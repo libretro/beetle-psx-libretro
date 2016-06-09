366a7311a90617031002cb25dd025e6a094f468f
twinaphex <libretro@gmail.com>
2016-06-09 03:56:43 +0200

    Update

--- libretro-common/string/stdstring.c
+++ libretro-common/string/stdstring.c
@@ -20,6 +20,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+#include <stdint.h>
 #include <ctype.h>
 
 #include <string/stdstring.h>
@@ -122,17 +123,13 @@ char *string_replace_substring(const char *in,
 void string_trim_whitespace_left(char *string)
 {
    bool in_whitespace  = true;
-   int32_t si          = 0;;
+   int32_t si          = 0;
    int32_t di          = 0;
 
    while(string[si])
    {
       bool test = in_whitespace && 
-            (string[si] == ' '  || 
-             string[si] == '\r' || 
-             string[si] == '\n' || 
-             string[si] == '\t' || 
-             string[si] == 0x0b);
+            isspace(string[si]);
 
       if(!test)
       {
@@ -143,7 +140,7 @@ void string_trim_whitespace_left(char *string)
 
       si++;
    }
-   string[di] = 0;
+   string[di] = '\0';
 }
 
 /* Remove whitespace from end of string */
@@ -156,14 +153,10 @@ void string_trim_whitespace_right(char *string)
       int32_t x;
       for(x = len - 1; x >= 0; x--)
       {
-         bool test = string[x] == ' '  || string[x] == '\r' 
-            || string[x] == '\n' || string[x] == '\t' 
-            || string[x] == 0x0b;
-
-         if (!test)
+         if (!isspace(string[x]))
             break;
 
-         string[x] = 0;
+         string[x] = '\0';
       }
    }
 }
