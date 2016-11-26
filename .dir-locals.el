69823e3be72c0fef99cd0daf13fbe995385b476a
Lionel Flandrin <lionel@svkt.org>
2016-11-26 15:27:50 +0100

    Added dir-locals.el files for emacs to indent correctly by default

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((c-mode . ((mode . c++)))
+ (nil . ((indent-tabs-mode . nil)
+         (c-basic-offset . 3)))
+ ("rustation-libretro" .
+  ((nil . ((indent-tabs-mode . nil)
+           (c-basic-offset . 4))))))
