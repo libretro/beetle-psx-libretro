bcba4111fa032d9e4cf4d7a5dc586ef5152ece9d
twinaphex <libretro@gmail.com>
2017-01-20 01:28:34 +0100

    (JNI Android Vulkan) Add Vulkan JNI Makefiles

--- parallel-psx/jni/Android.mk
+++ parallel-psx/jni/Android.mk
@@ -3,6 +3,7 @@ LOCAL_PATH := $(call my-dir)
 DEBUG = 0
 FRONTEND_SUPPORTS_RGB565 = 1
 FAST = 1
+HAVE_VULKAN=1
 
 include $(CLEAR_VARS)
 
@@ -23,7 +24,7 @@ endif
 LOCAL_CXXFLAGS += $(ANDROID_FLAGS)
 LOCAL_CFLAGS   += $(ANDROID_FLAGS)
 
-CORE_DIR        := ..
+CORE_DIR        := ../..
 LOCAL_MODULE    := libretro
 
 PTHREAD_FLAGS = -pthread
@@ -35,7 +36,7 @@ NEED_THREADING = 1
 NEED_TREMOR = 1
 CORE_DEFINE := -DWANT_PSX_EMU
 
-TARGET_NAME := mednafen_psx_libretro
+TARGET_NAME := mednafen_psx_hw_libretro
 
 include $(CORE_DIR)/Makefile.common
 
@@ -52,9 +53,12 @@ LDFLAGS += $(fpic) $(SHARED)
 FLAGS += $(fpic) $(NEW_GCC_FLAGS) $(INCFLAGS)
 
 FLAGS += $(ENDIANNESS_DEFINES) -DSIZEOF_DOUBLE=8 $(WARNINGS) -DMEDNAFEN_VERSION=\"0.9.26\" -DPACKAGE=\"mednafen\" -DMEDNAFEN_VERSION_NUMERIC=926 -DPSS_STYLE=1 -DMPC_FIXED_POINT $(CORE_DEFINE) -DSTDC_HEADERS -D__STDC_LIMIT_MACROS -D__LIBRETRO__ -DNDEBUG -D_LOW_ACCURACY_ $(SOUND_DEFINE) -D__STDC_CONSTANT_MACROS -DGIT_VERSION=\"$(GIT_VERSION)\" $(INCFLAGS)
+FLAGS += -DHAVE_VULKAN -DHAVE_HW
+
+LOCAL_C_INCLUDES = $(CORE_DIR)/parallel-psx $(CORE_DIR)/parallel-psx/atlas $(CORE_DIR)/parallel-psx/vulkan $(CORE_DIR)/parallel-psx/renderer $(CORE_DIR)/parallel-psx/khronos/include $(CORE_DIR)/parallel-psx/glsl/prebuilt $(CORE_DIR)/parallel-psx/vulkan/SPIRV-Cross $(CORE_DIR)/parallel-psx/vulkan/SPIRV-Cross/include
 
 LOCAL_CFLAGS =  $(FLAGS) 
-LOCAL_CXXFLAGS = $(FLAGS) -fexceptions
+LOCAL_CXXFLAGS = $(FLAGS) -fexceptions -std=c++11
 LOCAL_LDLIBS += -lz
 
 include $(BUILD_SHARED_LIBRARY)
