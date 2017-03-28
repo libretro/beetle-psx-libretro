8ca21c2fc3333ef557f02e0c49dddcfa23f86804
twinaphex <libretro@gmail.com>
2017-03-28 07:57:25 +0200

    Cleanups

--- rustation-libretro/src/retrogl/framebuffer.cpp
+++ rustation-libretro/src/retrogl/framebuffer.cpp
@@ -3,36 +3,32 @@
 #include <stdlib.h> // exit()
 #include <stdio.h>
 
-static void InitializeWithColorTexture(Framebuffer *fb, Texture* color_texture)
-{
-    GLuint id = 0;
-    glGenFramebuffers(1, &id);
-
-    fb->id = id;
-    fb->_color_texture = color_texture;
-
-    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fb->id);
-
-    glFramebufferTexture(   GL_DRAW_FRAMEBUFFER,
-                            GL_COLOR_ATTACHMENT0,
-                            color_texture->id,
-                            0);
-
-    GLenum col_attach_0 = GL_COLOR_ATTACHMENT0;
-    glDrawBuffers(1, &col_attach_0);
-    glViewport( 0,
-                0,
-                (GLsizei) color_texture->width,
-                (GLsizei) color_texture->height);
-
-#ifdef DEBUG
-    get_error();
-#endif
-}
-
 Framebuffer::Framebuffer(Texture* color_texture)
 {
-    InitializeWithColorTexture(this, color_texture);
+   Framebuffer *fb = this;
+   GLuint id = 0;
+   glGenFramebuffers(1, &id);
+
+   fb->id = id;
+   fb->_color_texture = color_texture;
+
+   glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fb->id);
+
+   glFramebufferTexture(   GL_DRAW_FRAMEBUFFER,
+         GL_COLOR_ATTACHMENT0,
+         color_texture->id,
+         0);
+
+   GLenum col_attach_0 = GL_COLOR_ATTACHMENT0;
+   glDrawBuffers(1, &col_attach_0);
+   glViewport( 0,
+         0,
+         (GLsizei) color_texture->width,
+         (GLsizei) color_texture->height);
+
+#ifdef DEBUG
+   get_error();
+#endif
 }
 
 Framebuffer::~Framebuffer()
