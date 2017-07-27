8a4846e1ff92c3c727aa63f01f662c6344b4f348
twinaphex <libretro@gmail.com>
2017-07-27 20:43:26 +0200

    Add log messages

--- rsx/rsx_lib_gl.cpp
+++ rsx/rsx_lib_gl.cpp
@@ -569,6 +569,14 @@ static bool Program_init(
    program->id       = id;
    program->uniforms = uniforms;
 
+   log_cb(RETRO_LOG_INFO, "Binding program for first time: %d\n", id);
+
+   glUseProgram(id);
+
+   log_cb(RETRO_LOG_INFO, "Unbinding program for first time: %d\n", id);
+
+   glUseProgram(0);
+
    return true;
 }
 
