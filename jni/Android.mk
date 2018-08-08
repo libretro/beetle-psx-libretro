7bb007de15b2f40a880b788ebf47a400c90827d9
Twinaphex <libretro@gmail.com>
2018-08-08 20:35:14 +0200

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -13,7 +13,7 @@ NEED_TREMOR              := 1
 GLES                     := 0
 HAVE_OPENGL              := 0
 HAVE_VULKAN              := 0
-HAVE_CHD                 := 0
+HAVE_CHD                 := 1
 IS_X86                   := 0
 FLAGS                    :=
 
