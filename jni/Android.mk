ba28561d1a75db79542f7c497193347a6d817598
twinaphex <libretro@gmail.com>
2018-12-28 02:26:09 +0100

    Should finally link

--- jni/Android.mk
+++ jni/Android.mk
@@ -62,5 +62,6 @@ LOCAL_SRC_FILES    := $(SOURCES_CXX) $(SOURCES_C)
 LOCAL_CFLAGS       := $(COREFLAGS)
 LOCAL_CXXFLAGS     := $(COREFLAGS) -std=c++11
 LOCAL_LDFLAGS      := -Wl,-version-script=$(CORE_DIR)/link.T -ldl
+LOCAL_LDLIBS       := -llog
 LOCAL_CPP_FEATURES := exceptions
 include $(BUILD_SHARED_LIBRARY)
