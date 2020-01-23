f4fc76cee8a628067c56ce9f313e8186d0e65b4a
Zachary Cook <zachcook1991@gmail.com>
2020-01-23 16:11:33 -0500

    git subrepo pull (merge) deps/lightrec
    subrepo:
      subdir:   "deps/lightrec"
      merged:   "3636435a"
    upstream:
      origin:   "https://github.com/pcercuei/lightrec.git"
      branch:   "master"
      commit:   "96b4f031"
    git-subrepo:
      version:  "0.4.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a04d8c2"

--- deps/lightrec/blockcache.c
+++ deps/lightrec/blockcache.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Paul Cercueil <paul@crapouillou.net>
+ * Copyright (C) 2015-2020 Paul Cercueil <paul@crapouillou.net>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
