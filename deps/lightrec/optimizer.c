36c8a9d33615cfca50ec81790c575ebb9cf6e1d3
Zachary Cook <zachcook1991@gmail.com>
2020-05-16 20:14:19 -0400

    Lightrec: Improve homebrew support
    git subrepo commit (merge) deps/lightrec
    
    subrepo:
      subdir:   "deps/lightrec"
      merged:   "b2c00115"
    upstream:
      origin:   "https://github.com/pcercuei/lightrec.git"
      branch:   "master"
      commit:   "2081869a"
    git-subrepo:
      version:  "0.4.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a04d8c2"

--- deps/lightrec/optimizer.c
+++ deps/lightrec/optimizer.c
@@ -877,11 +877,11 @@ static int lightrec_flag_stores(struct block *block)
 		case OP_SB:
 		case OP_SH:
 		case OP_SW:
-			/* Mark all store operations that target $sp, $gp, $k0
-			 * or $k1 as not requiring code invalidation. This is
-			 * based on the heuristic that stores using one of these
+			/* Mark all store operations that target $sp or $gp
+			 * as not requiring code invalidation. This is based
+			 * on the heuristic that stores using one of these
 			 * registers as address will never hit a code page. */
-			if (list->i.rs >= 26 && list->i.rs <= 29 &&
+			if (list->i.rs >= 28 && list->i.rs <= 29 &&
 			    !block->state->maps[PSX_MAP_KERNEL_USER_RAM].ops) {
 				pr_debug("Flaging opcode 0x%08x as not requiring invalidation\n",
 					 list->opcode);
