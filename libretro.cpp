bf5d7f866b1dde9a27c4ad0e6c23ddd8e69c048d
twinaphex <libretro@gmail.com>
2020-07-01 18:20:31 +0200

    Renamed state.cpp to state.c

--- libretro.cpp
+++ libretro.cpp
@@ -2405,7 +2405,7 @@ static void CDSelect(void)
    }
 }
 
-int StateAction(StateMem *sm, int load, int data_only)
+extern "C" int StateAction(StateMem *sm, int load, int data_only)
 {
    SFORMAT StateRegs[] =
    {
