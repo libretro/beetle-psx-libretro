a7c353c456655eb13676133f7d053c15620252d0
twinaphex <libretro@gmail.com>
2020-09-24 08:23:39 +0200

    Cleanups

--- mednafen/mednafen.h
+++ mednafen/mednafen.h
@@ -1,27 +1,15 @@
 #ifndef _MEDNAFEN_H
 #define _MEDNAFEN_H
 
-#include <libretro.h>
-#include "mednafen-types.h"
 #include <stdlib.h>
 #include <string.h>
 
-#define _(String) (String)
+#include <libretro.h>
+#include "mednafen-types.h"
 
 #ifdef _WIN32
 #define strcasecmp _stricmp
 #endif
-
-/* This breaks on OSX for some reason */
-#if 0
-#if __cplusplus <= 199711L
-# define HAS_CXX11
-#endif
-#endif
-
-#define GET_FDATA_PTR(fp) (fp->data)
-#define GET_FSIZE_PTR(fp) (fp->size)
-#define GET_FEXTS_PTR(fp) (fp->ext)
 
 void MDFND_DispMessage(
       unsigned priority, enum retro_log_level level,
