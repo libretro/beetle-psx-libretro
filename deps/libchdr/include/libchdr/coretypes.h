4d167109192cd4b71535c609e2564929a6195b3c
Alberto Fustinoni <alberto@aftnet.net>
2024-06-11 11:08:46 +0900

    VFS fix

--- deps/libchdr/include/libchdr/coretypes.h
+++ deps/libchdr/include/libchdr/coretypes.h
@@ -20,22 +20,31 @@ typedef int32_t INT32;
 typedef int16_t INT16;
 typedef int8_t INT8;
 
+#ifdef USE_LIBRETRO_VFS
+#define core_file RFILE
+#define core_fopen(file) rfopen(file, "rb")
+#define core_fseek rfseek
+#define core_ftell rftell
+#define core_fread(fc, buff, len) fread(buff, 1, len, fc)
+#define core_fclose rfclose
+#else /* USE_LIBRETRO_VFS */
 #define core_file FILE
 #define core_fopen(file) fopen(file, "rb")
 #if defined(__WIN32__) || defined(_WIN32) || defined(WIN32) || defined(__WIN64__)
-	#define core_fseek _fseeki64
-	#define core_ftell _ftelli64
+#define core_fseek _fseeki64
+#define core_ftell _ftelli64
 #elif defined(_LARGEFILE_SOURCE) && defined(_FILE_OFFSET_BITS) && _FILE_OFFSET_BITS == 64
-	#define core_fseek fseeko64
-	#define core_ftell ftello64
+#define core_fseek fseeko64
+#define core_ftell ftello64
 #else
-	#define core_fseek fseeko
-	#define core_ftell ftello
+#define core_fseek fseeko
+#define core_ftell ftello
 #endif
 #define core_fread(fc, buff, len) fread(buff, 1, len, fc)
 #define core_fclose fclose
+#endif /* USE_LIBRETRO_VFS */
 
-static UINT64 core_fsize(core_file *f)
+static UINT64 core_fsize(core_file* f)
 {
     UINT64 rv;
     UINT64 p = core_ftell(f);
