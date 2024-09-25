ca7c763f7f6077e34322e9e0d9bca9084425dad0
sonninnos <tony@pihaus.org>
2024-09-26 02:27:04 +0300

    Update libchdr + add zstd

--- deps/libchdr/include/libchdr/chdconfig.h
+++ deps/libchdr/include/libchdr/chdconfig.h
@@ -1,6 +1,8 @@
 #ifndef __CHDCONFIG_H__
 #define __CHDCONFIG_H__
 
+#include <retro_inline.h>
+
 /* Configure CHDR features here */
 #define WANT_RAW_DATA_SECTOR    1
 #define WANT_SUBCODE            1
